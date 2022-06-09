2d07edc49938c77e1771b0e9b4fbac35ccbc9b70
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-06-09 15:16:34 +0530

    Update linked.cpp

--- linked.cpp
+++ linked.cpp
@@ -6,15 +6,15 @@ int main()
 int f[50], p,i, st, len, j, c, k, a;
 for(i=0;i<50;i++)
 f[i]=0;
-printf("Enter how many blocks already allocated: ");
+printf("\nEnter how many blocks already allocated: ");
 scanf("%d",&p);
-printf("Enter blocks already allocated: ");
+printf("Enter blocks already allocated: \n");
 for(i=0;i<p;i++)
 {
 scanf("%d",&a);
 f[a]=1;
 }
-x: printf("Enter index starting block and length: ");
+x: printf("Enter index starting block and length: \n");
 scanf("%d%d", &st,&len);
 k=len;
 if(f[st]==0)
@@ -28,14 +28,14 @@ printf("%d ------->%d\n",j,f[j]);
 }
 else
 {
-printf("%d Block is already allocated \n",j);
+printf("\n\t%d Block is already allocated \n",j);
 k++;
 }
 }
 }
 else
-printf("%d starting block is already allocated \n",st);
-printf("Do you want to enter more file(Yes - 1/No - 0)");
+printf("\n\t%d starting block is already allocated \n",st);
+printf("\n\nDo you want to enter more file(Yes - 1/No - 0)");
 scanf("%d", &c);
 if(c==1)
 goto x;
